a2e2b669481278813e647e2aa81de915a6536e65
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-08-03 22:15:50 -0300

    Fix: passing all tests

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,9 +1,10 @@
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+from collections.abc import Iterable
 
 
-class InventoryRefactor:
+class InventoryRefactor(Iterable):
     def __init__(self, importer):
         self.importer = importer
         self.data = []
@@ -11,7 +12,7 @@ class InventoryRefactor:
     def __iter__(self):
         return InventoryIterator(self.data)
 
-    def import_data(self, path, report_type):
+    def import_data(self, path, type):
         self.data.extend(self.importer.import_data(path))
 
         if type == "simples":
